defc0bb9c5356c66d5f43564a3e5bfb4d0e4e9f5
Connor Bell <cjbell630@users.noreply.github.com>
2020-09-17 20:27:06 -0400

    Added functions

--- fceux/emu.lua
+++ fceux/emu.lua
@@ -55,10 +55,116 @@ end
 
 ---message: Displays given message on screen in the standard messages position. Use [gui.text()][gui.lua] when you need to position text.
 ---@param message string
-function emu.message(message) end
+function emu.message(message)
+end
 
----framecount
+---framecount: Returns the framecount value. The frame counter runs without a movie running so this always returns a value.
 ---@return number (int)
-function emu.framecount() return  end
-return emu
+function emu.framecount()
+end
 
+---lagcount: Returns the number of lag frames encountered. Lag frames are frames where the game did not poll for input because it missed the vblank. This happens when it has to compute too much within the frame boundary. This returns the number indicated on the lag counter.
+---@return number (int)
+function emu.lagcount()
+end
+
+---lagged: Returns true if currently in a lagframe, false otherwise.
+---@return boolean
+function emu.lagged()
+end
+
+---setlagflag: Sets current value of lag flag.
+---Some games poll input even in lag frames, so standard way of detecting lag (used by FCEUX and other emulators) does not work for those games, and you have to determine lag frames manually.
+---First, find RAM addresses that help you distinguish between lag and non-lag frames (e.g. an in-game frame counter that only increments in non-lag frames). Then register memory hooks that will change lag flag when needed.
+---@param value boolean
+function emu.setlagflag(value)
+end
+
+---emulating: Returns true if emulation has started, or false otherwise. Certain operations such as using savestates are invalid to attempt before emulation has started. You probably won't need to use this function unless you want to make your script extra-robust to being started too early.
+---@return boolean
+function emu.emulating()
+end
+
+---paused: Returns true if emulator is paused, false otherwise.
+---@return boolean
+function emu.paused()
+end
+
+---readonly: Returns whether the emulator is in read-only state.
+---While this variable only applies to movies, it is stored as a global variable and can be modified even without a movie loaded.  Hence, it is in the emu library rather than the movie library.
+---@alias movie.readonly
+---@return boolean fortnite
+function emu.readonly()
+end
+
+---setreadonly: Sets the read-only status to read-only if argument is true and read+write if false.
+---Note: This might result in an error if the medium of the movie file is not writeable (such as in an archive file).
+---While this variable only applies to movies, it is stored as a global variable and can be modified even without a movie loaded.  Hence, it is in the emu library rather than the movie library.
+---@alias movie.setreadonly
+---@param state boolean
+function emu.setreadonly(state)
+end
+
+---getdir: Returns the path of fceux.exe as a string.
+---@return string
+function emu.getdir()
+end
+
+---loadrom: Loads the ROM from the directory relative to the lua script or from the absolute path. Hence, the filename parameter can be absolute or relative path.
+---If the ROM can't be loaded, loads the most recent one.
+---@param filename string
+function emu.loadrom(filename)
+end
+
+---registerbefore: Registers a callback function to run immediately before each frame gets emulated. This runs after the next frame's input is known but before it's used, so this is your only chance to set the next frame's input using the next frame's would-be input. For example, if you want to make a script that filters or modifies ongoing user input, such as making the game think "left" is pressed whenever you press "right", you can do it easily with this.
+---Note that this is not quite the same as code that's placed before a call to emu.frameadvance. This callback runs a little later than that. Also, you cannot safely assume that this will only be called once per frame. Depending on the emulator's options, every frame may be simulated multiple times and your callback will be called once per simulation. If for some reason you need to use this callback to keep track of a stateful linear progression of things across frames then you may need to key your calculations to the results of emu.framecount.
+---Like other callback-registering functions provided by FCEUX, there is only one registered callback at a time per registering function per script. If you register two callbacks, the second one will replace the first, and the call to emu.registerbefore will return the old callback. You may register nil instead of a function to clear a previously-registered callback. If a script returns while it still has registered callbacks, FCEUX will keep it alive to call those callbacks when appropriate, until either the script is stopped by the user or all of the callbacks are de-registered.
+---@param func function
+function emu.registerbefore(func)
+end
+
+---registerafter: Registers a callback function to run immediately after each frame gets emulated. It runs at a similar time as (and slightly before) gui.register callbacks, except unlike with gui.register it doesn't also get called again whenever the screen gets redrawn. Similar caveats as those mentioned in emu.registerbefore apply.
+---@param func function
+function emu.registerafter(func)
+end
+
+---registerexit: Registers a callback function that runs when the script stops. Whether the script stops on its own or the user tells it to stop, or even if the script crashes or the user tries to close the emulator, FCEUX will try to run whatever Lua code you put in here first. So if you want to make sure some code runs that cleans up some external resources or saves your progress to a file or just says some last words, you could put it here. (Of course, a forceful termination of the application or a crash from inside the registered exit function will still prevent the code from running.)
+---Suppose you write a script that registers an exit function and then enters an infinite loop. If the user clicks "Stop" your script will be forcefully stopped, but then it will start running its exit function. If your exit function enters an infinite loop too, then the user will have to click "Stop" a second time to really stop your script. That would be annoying. So try to avoid doing too much inside the exit function.
+---Note that restarting a script counts as stopping it and then starting it again, so doing so (either by clicking "Restart" or by editing the script while it is running) will trigger the callback. Note also that returning from a script generally does NOT count as stopping (because your script is still running or waiting to run its callback functions and thus does not stop... see here for more information), even if the exit callback is the only one you have registered.
+---@param func function
+function emu.registerexit(func)
+end
+
+---addgamegenie: Adds a Game Genie code to the Cheats menu. Returns false and an error message if the code can't be decoded. Returns false if the code couldn't be added. Returns true if the code already existed, or if it was added.
+---Usage: emu.addgamegenie("NUTANT")
+---Note that the Cheats Dialog Box won't show the code unless you close and reopen it.
+---@param str string
+---@return boolean
+function emu.addgamegenie(str)
+end
+
+---delgamegenie: Removes a Game Genie code from the Cheats menu. Returns false and an error message if the code can't be decoded. Returns false if the code couldn't be deleted. Returns true if the code didn't exist, or if it was deleted.
+---Usage: emu.delgamegenie("NUTANT")
+---Note that the Cheats Dialog Box won't show the code unless you close and reopen it.
+---@param str string
+function emu.delgamegenie(str)
+end
+
+---print: Puts a message into the Output Console area of the Lua Script control window. Useful for displaying usage instructions to the user when a script gets run.
+---@param str string
+function emu.print(str)
+end
+
+
+--TODO: says it returns something in the desc but not in the header
+---getscreenpixel: Returns the separate RGB components of the given screen pixel, and the palette. Can be 0-255 by 0-239, but NTSC only displays 0-255 x 8-231 of it. If getemuscreen is false, this gets background colors from either the screen pixel or the LUA pixels set, but LUA data may not match the information used to put the data to the screen. If getemuscreen is true, this gets background colors from anything behind an LUA screen element.
+---Usage is local r,g,b,palette = emu.getscreenpixel(5, 5, false) to retrieve the current red/green/blue colors and palette value of the pixel at 5x5.
+---Palette value can be 0-63, or 254 if there was an error.
+---You can avoid getting LUA data by putting the data into a function, and feeding the function name to emu.registerbefore.
+---@param x number (int)
+---@param y number (int)
+---@param getemuscreen boolean
+---@return number, number, number, number
+function emu.getscreenpixel(x, y, getemuscreen)
+end
+return emu
