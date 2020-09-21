edeaafedea33fff5f634ff9a9f70bfe559f99389
Connor Bell <cjbell630@users.noreply.github.com>
2020-09-21 19:36:38 -0400

    Output changed to a poorly written HTML file that shows the names and return types of all functions.

--- example.lua
+++ example.lua
@@ -8,79 +8,87 @@
     --["rom.getfilename"] = {rom.getfilename() --REMOVED
 }]]
 
-prop_form_funcs= {
-    ["emu.exec_count"] = {emu.exec_count(1000, print(""))},
-    ["emu.loadrom"] = {emu.loadrom("romname")},
-    ["emu.lagged"] = {emu.lagged()},
-    ["emu.readonly"] = {emu.readonly()},
-    ["emu.lagcount"] = {emu.lagcount()},
-    ["emu.print"] = {emu.print("string")},
-    ["emu.getscreenpixel"] = {emu.getscreenpixel(15, 15, true)},
-    ["emu.message"] = {emu.message("string")},
-    ["emu.getdir"] = {emu.getdir()},
-    ["emu.setlagflag"] = {emu.setlagflag(true)},
-    ["emu.setrenderplanes"] = {emu.setrenderplanes(true, true)},
-    ["emu.setreadonly"] = {emu.setreadonly(true)},
-    ["emu.emulating"] = {emu.emulating()},
-    ["emu.addgamegenie"] = {emu.addgamegenie("string")},
-    ["emu.frameadvance"] = {emu.frameadvance()},
-    ["emu.poweron"] = {emu.poweron()},
-    ["emu.pause"] = {emu.pause()},
-    ["emu.registerexit"] = {emu.registerexit(print)},
-    ["emu.unpause"] = {emu.unpause()},
-    ["emu.registerafter"] = {emu.registerafter(print)},
-    ["emu.registerbefore"] = {emu.registerbefore(print)},
-    ["emu.softreset"] = {emu.softreset()},
-    ["emu.paused"] = {emu.paused()},
-    ["emu.framecount"] = {emu.framecount()},
-    ["emu.speedmode"] = {emu.speedmode("normal")},
-    ["emu.delgamegenie"] = {emu.delgamegenie("string")},
+--[[funcs = {
+    ["emu.exec_count"] = { emu.exec_count(1000, print("")) },
+    ["emu.loadrom"] = { emu.loadrom("romname") },
+    ["emu.lagged"] = { emu.lagged() },
+    ["emu.readonly"] = { emu.readonly() },
+    ["emu.lagcount"] = { emu.lagcount() },
+    ["emu.print"] = { emu.print("string") },
+    ["emu.getscreenpixel"] = { emu.getscreenpixel(15, 15, true) },
+    ["emu.message"] = { emu.message("string") },
+    ["emu.getdir"] = { emu.getdir() },
+    ["emu.setlagflag"] = { emu.setlagflag(true) },
+    ["emu.setrenderplanes"] = { emu.setrenderplanes(true, true) },
+    ["emu.setreadonly"] = { emu.setreadonly(true) },
+    ["emu.emulating"] = { emu.emulating() },
+    ["emu.addgamegenie"] = { emu.addgamegenie("string") },
+    ["emu.frameadvance"] = { emu.frameadvance() },
+    ["emu.poweron"] = { emu.poweron() },
+    ["emu.pause"] = { emu.pause() },
+    ["emu.registerexit"] = { emu.registerexit(print) },
+    ["emu.unpause"] = { emu.unpause() },
+    ["emu.registerafter"] = { emu.registerafter(print) },
+    ["emu.registerbefore"] = { emu.registerbefore(print) },
+    ["emu.softreset"] = { emu.softreset() },
+    ["emu.paused"] = { emu.paused() },
+    ["emu.framecount"] = { emu.framecount() },
+    ["emu.speedmode"] = { emu.speedmode("normal") },
+    ["emu.delgamegenie"] = { emu.delgamegenie("string") },
 
-    ["rom.readbyteunsigned"] = {rom.readbyteunsigned(0xF0F)},
-    ["rom.readbytesigned"] = {rom.readbytesigned(0xF0F)},
-    ["rom.writebyte"] = {rom.writebyte(0xF0F, 0xD)},
-    ["rom.readbyte"] = {rom.readbyte(0xF0F)},
-    ["rom.gethash"] = {rom.gethash("md5")},
+    ["rom.readbyteunsigned"] = { rom.readbyteunsigned(0xF0F) },
+    ["rom.readbytesigned"] = { rom.readbytesigned(0xF0F) },
+    ["rom.writebyte"] = { rom.writebyte(0xF0F, 0xD) },
+    ["rom.readbyte"] = { rom.readbyte(0xF0F) },
+    ["rom.gethash"] = { rom.gethash("md5") },
 
-
-    ["memory.writebyte"] = {memory.writebyte(0xF0F, 15)},
-    ["memory.registerexec"] = {memory.registerexec(0xF0F, print)},
-    ["memory.registerexec 3 params"] = {memory.registerexec(0xF0F, 9, print)},
-    ["memory.readbyte"] = {memory.readbyte(0xF0F)},
-    ["memory.registerrun"] = {memory.registerrun(0xF0F, print)},
-    ["memory.registerrun 3 params"] = {memory.registerrun(0xF0F, 9, print)},
-    ["memory.readword"] = {memory.readword(0xF0F)},
-    ["memory.readword 2 params"] = {memory.readword(0xF0F, 0xFFF)},
-    ["memory.getregister"] = {memory.getregister("pc")},
-    ["memory.register"] = {memory.register(0xF0F, print)},
-    ["memory.register 3 params"] = {memory.register(0xF0F, 9, print)},
-    ["memory.registerexecute"] = {memory.registerexecute(0xF0F, print)},
-    ["memory.registerexecute 3 params"] = {memory.registerexecute(0xF0F, 9, print)},
-    ["memory.readbyterange"] = {memory.readbyterange(0xF0F, 4)},
-    ["memory.readwordsigned"] = {memory.readwordsigned(0xF0F)},
-    ["memory.readwordsigned 2 params"] = {memory.readwordsigned(0xF0F, 0xFFF)},
-    ["memory.registerwrite"] = {memory.registerwrite(0xF0F, print)},
-    ["memory.registerwrite 3 params"] = {memory.registerwrite(0xF0F, 9, print)},
-    ["memory.readwordunsigned"] = {memory.readwordunsigned(0xF0F)},
-    ["memory.readwordunsigned 2 params"] = {memory.readwordunsigned(0xF0F, 0xFFF)},
-    ["memory.readbytesigned"] = {memory.readbytesigned(0xF0F)},
-    ["memory.readbyteuunsigned"] = {memory.readbyteunsigned(0xF0F)},
-    ["memory.setregister"] = {memory.setregister("pc", 0x200)}
-}
+    ["memory.writebyte"] = { memory.writebyte(0xF0F, 15) },
+    ["memory.registerexec"] = { memory.registerexec(0xF0F, print) },
+    ["memory.registerexec 3 params"] = { memory.registerexec(0xF0F, 9, print) },
+    ["memory.readbyte"] = { memory.readbyte(0xF0F) },
+    ["memory.registerrun"] = { memory.registerrun(0xF0F, print) },
+    ["memory.registerrun 3 params"] = { memory.registerrun(0xF0F, 9, print) },
+    ["memory.readword"] = { memory.readword(0xF0F) },
+    ["memory.readword 2 params"] = { memory.readword(0xF0F, 0xFFF) },
+    ["memory.getregister"] = { memory.getregister("pc") },
+    ["memory.register"] = { memory.register(0xF0F, print) },
+    ["memory.register 3 params"] = { memory.register(0xF0F, 9, print) },
+    ["memory.registerexecute"] = { memory.registerexecute(0xF0F, print) },
+    ["memory.registerexecute 3 params"] = { memory.registerexecute(0xF0F, 9, print) },
+    ["memory.readbyterange"] = { memory.readbyterange(0xF0F, 4) },
+    ["memory.readwordsigned"] = { memory.readwordsigned(0xF0F) },
+    ["memory.readwordsigned 2 params"] = { memory.readwordsigned(0xF0F, 0xFFF) },
+    ["memory.registerwrite"] = { memory.registerwrite(0xF0F, print) },
+    ["memory.registerwrite 3 params"] = { memory.registerwrite(0xF0F, 9, print) },
+    ["memory.readwordunsigned"] = { memory.readwordunsigned(0xF0F) },
+    ["memory.readwordunsigned 2 params"] = { memory.readwordunsigned(0xF0F, 0xFFF) },
+    ["memory.readbytesigned"] = { memory.readbytesigned(0xF0F) },
+    ["memory.readbyteuunsigned"] = { memory.readbyteunsigned(0xF0F) },
+    ["memory.setregister"] = { memory.setregister("pc", 0x200) }
+}]]
 
 --[[for k, v in pairs(memory) do
     print(k)
 end]]--
 
-for name, result in pairs(prop_form_funcs) do
-    file = io.open("D:\\cb106\\Documents\\GitHub\\FCEUX-Lua-API\\output.txt", "a")
+file = io.open("D:\\cb106\\Documents\\GitHub\\FCEUX-Lua-API\\output.html", "w+")
+file:write("<html><head><style>table {border-collapse: collapse;width: 100%;border: 1px solid black;}th, td {text-align: left;padding: 8px;border: 1px solid black;}tr:nth-child(even) {background-color: #d0d0d0;border: 1px solid black;}</style></head><body><table style=\"width:100%\">")
+file:close()
+file = io.open("D:\\cb106\\Documents\\GitHub\\FCEUX-Lua-API\\output.html", "a")
+for name, result in pairs(funcs) do
+    file:write("<tr><td>")
     file:write(name)
-    file:write(": ")
-    for i, _ in pairs(result) do
-        file:write((i == nil) and "no return value" or tostring(i) .. " .. " .. type(i)..",  ")
+    file:write("</td>")
+    no_return_value = true
+    for _, i in pairs(result) do
+        file:write("<td>")
+        file:write((i == nil) and "this shouldn't be here" or type(i) .. ": " .. tostring(i))
+        file:write("</td>")
+        no_return_value = false
     end
-    file:write("\n")
-    file:close()
+    file:write(no_return_value and "<td><p style=\"color:TOMATO;\">no return value</p></td></tr>" or "</tr>")
 end
+file:write("</table></body></html>")
+file:close()
 print("done")
 
